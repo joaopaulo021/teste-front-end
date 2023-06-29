c943622333d924dce69732ec8a77ddf5f02cb3d8
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-06-29 15:59:06 -0300

    feat: Add App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import Aside from './components/Aside'
-import Navbar from './components/Navbar'
 import './styles.scss'
 
-const App = () => {
+import Aside from './components/Aside'
+import Main from './components/Main'
+import Navbar from './components/Navbar'
+import Register from './pages/register'
+
+import { useState } from 'react'
+import { FormValues } from './interfaces/interface';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+
+
+const setLocalStorage = (items: FormValues[]) => {
+  localStorage.setItem('bookings', JSON.stringify(items));
+}
+const defaultList: FormValues[] = JSON.parse(localStorage.getItem('bookings') || '[]');
+
+
+const App: React.FC = () => {
+
+  const [data, setData] = useState<FormValues[]>(defaultList);
+
+  const addItem = (reservaData: FormValues) => {
+    const newItem: FormValues = {
+      ...reservaData
+    };
+    setData(prevData => [...prevData, newItem]);
+    setLocalStorage([...data, newItem])
+  }
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main data={data} />,
+    },
+    {
+      path: "cadastro",
+      element: <Register addItem={addItem} />,
+    },
+  ]);
+
   return (
     <>
       <Navbar />
-      <Aside />
+      <main className='container'>
+        <Aside />
+        <RouterProvider router={router} />
+      </main>
     </>
   )
 }
