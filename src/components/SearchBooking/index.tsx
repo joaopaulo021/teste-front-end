92e1fe5a51b0c8d746931e6dcbf7ad26261f3665
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-06-29 15:52:08 -0300

    feat: Add SearchBooking component

---
+++ src/components/SearchBooking/index.tsx
@@ -0,0 +1,13 @@
+import './styles.scss'
+import { FiSearch } from 'react-icons/fi'
+
+const SearchBooking = () => {
+  return (
+    <section className="search-container">
+      <input type="text" />
+      <FiSearch />
+    </section>
+  )
+}
+
+export default SearchBooking
