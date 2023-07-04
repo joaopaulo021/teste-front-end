79e9730ae73220d185d97ce0230bdb01edb8fdaa
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-07-04 15:09:32 -0300

    feat: Add Modal and formatDate function

--- src/components/Booking/index.tsx
+++ src/components/Booking/index.tsx
@@ -2,12 +2,15 @@ import './styles.scss'
 import { BookingProps } from '../../interfaces/interface';
 import { format } from 'date-fns';
 import { AiOutlineEllipsis } from 'react-icons/ai'
+import { useState } from 'react'
 
 import img1 from '../../assets/accommodations/Apartamento-01.jpg'
 import img2 from '../../assets/accommodations/Casal-Premium.jpg'
 import img3 from '../../assets/accommodations/Chale-Aconchego.jpg'
 import img4 from '../../assets/accommodations/Duplo-Luxo.jpg'
 import img5 from '../../assets/accommodations/Standard-Casal.jpg'
+
+import Modal from '../Modal';
 
 const accommodations: { [key: string]: string } = {
   'Apartamento': img1,
@@ -21,37 +24,54 @@ const getImagePath = (acomodacao: string) => {
   return accommodations[acomodacao] || '';
 };
 
-const Booking: React.FC<BookingProps> = ({ item }) => {
-  const currentDate = format(new Date(), 'dd/MM/yyyy');
-  return (
 
+const Booking: React.FC<BookingProps> = ({ item, deleteItem }) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen((prevIsModalOpen) => !prevIsModalOpen);
+  };
+
+  const formatDateTime = (dateTime: string) => {
+    if (!dateTime) return '';
+
+    const formattedDate = format(new Date(dateTime), 'dd/MM/yyyy');
+    const formattedTime = format(new Date(dateTime), 'HH:mm');
+
+    return `${formattedDate}\n${formattedTime}`;
+  };
+
+  return (
     <section className='booking'>
       <div className="booking-img">
         <img src={getImagePath(item.acomodacao)} alt="" />
         <div className="description">
-          <p>{'#' + item.id}</p>
+          <button onClick={toggleModal}>{'#' + item.id}</button>
           <p>{item.acomodacao}</p>
         </div>
       </div>
 
       <div className="booking-item">
-        <h4>{item.nome}</h4>
+        <h4>{`${item.nome} ${item.sobrenome}`}</h4>
         <p>{item.hospedes}</p>
       </div>
 
       <div className="booking-item">
-        <p>{currentDate}</p>
-        <h4>{item.checkIn}</h4>
+        <span>{formatDateTime(item.checkIn)}</span>
       </div>
 
       <div className="booking-item">
-        <p>21/07/2023</p>
-        <h4>{item.checkOut}</h4>
+        <span>{formatDateTime(item.checkOut)}</span>
       </div>
-      <button type='button'>
+
+      <button className='btn-modal' type='button' onClick={toggleModal}>
         <AiOutlineEllipsis />
       </button>
 
+      {isModalOpen && (
+        <Modal deleteItem={deleteItem} getImagePath={getImagePath} item={item} toggleModal={toggleModal} />
+      )}
     </section>
   )
 }
