b8dccd2a8038659cd26d55b4eabb5433ef3d77ad
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-07-04 14:59:58 -0300

    feat: Add addItem, editItem, deleteItem functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './styles.scss'
 
 import Aside from './components/Aside'
-import Main from './components/Main'
 import Navbar from './components/Navbar'
 import Register from './pages/register'
+import Bookings from './components/Bookings'
+import { FormValues, SortCriteria } from './interfaces/interface';
 
 import { useState } from 'react'
-import { FormValues } from './interfaces/interface';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 
 
@@ -15,10 +15,18 @@ const setLocalStorage = (items: FormValues[]) => {
 }
 const defaultList: FormValues[] = JSON.parse(localStorage.getItem('bookings') || '[]');
 
-
 const App: React.FC = () => {
 
   const [data, setData] = useState<FormValues[]>(defaultList);
+  // const [sortCriteria, setSortCriteria] = useState<SortCriteria | null>(null);
+
+  // const sortData = (criteria: SortCriteria) => {
+  //   setSortCriteria(criteria);
+  //   const sortedData = [...data].sort((a, b) =>
+  //     a[criteria].localeCompare(b[criteria])
+  //   );
+  //   setData(sortedData);
+  // };
 
   const addItem = (reservaData: FormValues) => {
     const newItem: FormValues = {
@@ -28,14 +36,53 @@ const App: React.FC = () => {
     setLocalStorage([...data, newItem])
   }
 
+  const editItem = (updatedItem: FormValues) => {
+    const updatedData = data.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      }
+      return item;
+    });
+    setData(updatedData);
+    setLocalStorage(updatedData);
+  };
+
+  const deleteItem = (id: string) => {
+    const newItems = data.filter((item) => item.id !== id)
+    setData(newItems)
+    setLocalStorage(newItems)
+  }
+
+  // const handleSearch = (searchItem: string) => {
+  //   const filteredData = data.filter((item) => {
+  //     const searchTerm = searchItem.toLowerCase();
+  //     const acomodacao = item.acomodacao.toLowerCase();
+  //     const data = item.checkIn.toLowerCase();
+  //     const nomeHospede = item.nome.toLowerCase();
+
+  //     return acomodacao.includes(searchTerm) || data.includes(searchTerm) || nomeHospede.includes(searchTerm);
+  //   });
+  //   setData(filteredData);
+  // }
+
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Main data={data} />,
+      element:
+        <Bookings
+          sortData={sortData}
+          handleSearch={handleSearch}
+          deleteItem={deleteItem}
+          data={data}
+        />,
     },
     {
       path: "cadastro",
-      element: <Register addItem={addItem} />,
+      element:
+        <Register
+          editItem={editItem}
+          addItem={addItem}
+        />,
     },
   ]);
 
