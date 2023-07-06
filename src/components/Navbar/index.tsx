cec27bb0630157f87d981ce4be19610447e6995c
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-07-06 10:43:46 -0300

    feat: Add showMenu function

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,24 +1,40 @@
 import { FiHelpCircle, FiMail, FiSettings, FiChevronDown } from 'react-icons/fi'
 import logo from "../../assets/logo.png"
 import './styles.scss'
+import { useState } from 'react'
 
 const Navbar: React.FC = () => {
+
+  const [showMenu, setIsShowMenu] = useState(false)
+
   return (
     <header>
-      <div>
-        <img src={logo} alt="logo" />
-      </div>
-      <div className="menu-container">
+      <span>
+        <a href="/">
+          <img src={logo} alt="logo" />
+        </a>
+      </span>
+
+      <div className="icons-container">
         <section className="icons">
           <FiHelpCircle />
           <FiMail />
           <FiSettings />
         </section>
+      </div>
+      <div className="menu-container">
         <section className="login">
           <p>Admin</p>
-          <span>PF</span>
-          <FiChevronDown />
+          <span>JP</span>
+          <FiChevronDown onClick={() => setIsShowMenu(!showMenu)} className='login-btn' />
         </section>
+        {showMenu &&
+          <div className='menu'>
+            <a href="/logout">Logout</a>
+            <p>Trocar user</p>
+          </div>
+        }
+
       </div>
     </header>
   )
