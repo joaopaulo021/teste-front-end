61edd671c9fc5288af9bfb909b950de3bf30887f
João Paulo L. Gonçalves <107564968+joaopaulo021@users.noreply.github.com>
2023-07-06 10:42:51 -0300

    feat: Add search with keypress function

--- src/components/SearchBooking/index.tsx
+++ src/components/SearchBooking/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles.scss'
 import { FiPlus, FiSearch } from 'react-icons/fi'
 import { SearchProps } from '../../interfaces/interface'
@@ -6,30 +6,64 @@ import { SearchProps } from '../../interfaces/interface'
 const SearchBooking: React.FC<SearchProps> = ({ handleSearch }) => {
 
   const [searchItem, setSearchItem] = useState('')
+  const [selectedLink, setSelectedLink] = useState('');
 
+  const handleLinkClick = (link: string) => {
+    setSelectedLink(link);
+    localStorage.setItem('selectedLink', link);
+  };
+
+  useEffect(() => {
+    const storedLink = localStorage.getItem('selectedLink');
+    if (storedLink) {
+      setSelectedLink(storedLink);
+    }
+  }, []);
+
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      handleSearch(searchItem);
+    }
+  }
 
   return (
     <section className="search-container">
       <div className="link-search-container">
         <span>
-          <a href="/">Todas as reservas</a>
-          <a href="#">Carrinho</a>
+          <a
+            href="/"
+            className={selectedLink === 'reservas' ? 'selected' : ''}
+            onClick={() => handleLinkClick('reservas')}
+          >
+            Todas as reservas
+          </a>
+          <a
+            href="#"
+            className={selectedLink === 'carrinho' ? 'selected' : ''}
+            onClick={() => handleLinkClick('carrinho')}
+          >
+            Carrinho
+          </a>
         </span>
-
         <div className="search">
           <input
+            onKeyDown={handleKeyPress}
             placeholder='Pesquisar'
             onChange={(e) => setSearchItem(e.target.value)}
             type="text" />
           <button onClick={() => handleSearch(searchItem)} type='button'><FiSearch /></button>
         </div>
 
-        <button type='button'>
-          <FiPlus />
-          <p>Nova reserva</p>
-        </button>
+        <div className="new-booking">
+          <a href="/cadastro">
+            <button type='button'>
+              <FiPlus />
+              <p>Nova reserva</p>
+            </button>
+          </a>
+        </div>
       </div>
-    </section>
+    </section >
   )
 }
 
